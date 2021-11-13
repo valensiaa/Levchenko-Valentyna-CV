8cdbfc2f433dd7772118e6a552418b9a35e7bf67
valensiaa <valyaproject@gmail.com>
2021-11-13 14:35:40 +0200

    add project to github

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+let activeWrapper = document.querySelector('.wrapper');
+let popup = document.querySelector('.popup__bg');
+
+function openMain() {
+   activeWrapper.classList.remove('hide'); 
+    popup.classList.add('hide');
+}
