2ee243548461c8281e2953ce254a3900250ef524
Valentyna Levchenko <valyaproject@gmail.com>
2021-12-01 20:09:34 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-let activeWrapper = document.querySelector('.wrapper');
-let popup = document.querySelector('.popup');
+// let activeWrapper = document.querySelector('.wrapper');
+// let popup = document.querySelector('.popup');
 
-function openMain() {
-   activeWrapper.classList.remove('hide'); 
-    popup.classList.add('hide');
-}
+// function openMain() {
+//    activeWrapper.classList.remove('hide'); 
+//     popup.classList.add('hide');
+// }
