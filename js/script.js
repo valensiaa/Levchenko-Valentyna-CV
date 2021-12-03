3236ebb2bdd5f2aa66b469aa06045266d70a67b1
Valentyna Levchenko <valyaproject@gmail.com>
2021-12-03 11:54:06 +0200

    delete popup

--- js/script.js
+++
@@ -1,8 +0,0 @@
-
-// let activeWrapper = document.querySelector('.wrapper');
-// let popup = document.querySelector('.popup');
-
-// function openMain() {
-//    activeWrapper.classList.remove('hide'); 
-//     popup.classList.add('hide');
-// }
